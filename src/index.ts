54d6f9893e55b0da57f27b93f9b41700908cd6c3
Laszlo Hornyak <laszlo.hornyak@superwert.com>
2025-11-04 20:32:27 +0100

    reset-db endpoint moved

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-import { readFileSync } from 'node:fs'
 import { OpenAPIHono } from '@hono/zod-openapi'
 import {
   PrismaClientInitializationError,
@@ -12,7 +11,7 @@ import { HTTPException } from 'hono/http-exception'
 import { logger } from 'hono/logger'
 import books from './books/routes'
 import genres from './genres/routes'
-import prisma from './lib/prisma'
+import resetDb from './reset-db/routes'
 import reviews from './reviews/routes'
 
 const app = new OpenAPIHono()
@@ -30,21 +29,7 @@ const routes = app
 app.route('/books', books)
 app.route('/reviews', reviews)
 app.route('/genres', genres)
-app.post('/reset-db', async (c) => {
-  // Read the SQL file
-  const sql = readFileSync('seed.sql', 'utf8')
-
-  // Execute the SQL commands using Prisma's raw query method
-  const statements = sql
-    .split(';')
-    .map((s) => s.trim())
-    .filter(Boolean)
-
-  for (const stmt of statements) {
-    await prisma.$executeRawUnsafe(stmt)
-  }
-  return c.json({ status: 'ok' })
-})
+app.route('/reset-db', resetDb)
 
 app.onError((err, c) => {
   if (err instanceof HTTPException) {
